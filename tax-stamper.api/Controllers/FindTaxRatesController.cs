9ccd413986a9827c02a97450cbd245273ffdb3d4
randomsilo <randomsilo@gmail.com>
2019-07-05 22:34:43 -0500

    setting up the web

---
+++ tax-stamper.api/Controllers/FindTaxRatesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace tax_stamper.api.Controllers
+{
+    [ApiController]
+    public class FindTaxRatesController : ControllerBase
+    {
+
+        [HttpGet("api/usetaxratesusa")]
+        public void GetUseTaxRatesUSA()
+        {
+            
+        }
+
+    }
+}
--- tax-stamper.api/Startup.cs
+++ tax-stamper.api/Startup.cs
@@ -39,9 +39,9 @@ namespace tax_stamper.api
             {
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+                app.UseHttpsRedirection();
             }
 
-            app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
